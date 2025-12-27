a530b1b3d5173e484d0a97a8533a620b6dfbddf7
yaroslavsdev <yaroslav.safronov.09@yandex.ru>
2025-12-27 11:29:31 +0300

    navigation up on GoodsDetails.kt

--- app/src/main/java/com/example/lyceum_saturday10_2025/features/goodsdetails/GoodsDetails.kt
+++ app/src/main/java/com/example/lyceum_saturday10_2025/features/goodsdetails/GoodsDetails.kt
@@ -1,15 +1,17 @@
 package com.example.lyceum_saturday10_2025.features.goodsdetails
 
-import android.widget.Space
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,24 +24,37 @@ import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.example.lyceum_saturday10_2025.features.goods.presentation.model.GoodsItem
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 @Destination
-//@Destination(style = DestinationStyle.Dialog::class)
-fun GoodsDetails(goodsCurrent: GoodsItem) {
+fun GoodsDetails(navigator: DestinationsNavigator, goodsCurrent: GoodsItem) {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Card (
+        Card(
             modifier = Modifier
-                .padding(start = 10.dp, end = 10.dp)   // Outer padding
+                .padding(start = 10.dp, end = 10.dp)
                 .heightIn(50.dp, 600.dp)
         ) {
             Column(
                 modifier = Modifier.padding(16.dp)
             ) {
+                // Кнопка назад
+                IconButton(
+                    onClick = { navigator.navigateUp() },
+                    modifier = Modifier.size(40.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Назад"
+                    )
+                }
+
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+
                 GlideImage(
                     model = goodsCurrent.imageURL,
                     contentScale = ContentScale.FillWidth,
